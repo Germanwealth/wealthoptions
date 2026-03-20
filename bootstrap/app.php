ae69c07aea32c68d7377dce9052b08812ab7bd39
DjCeejay <lordceejay12@gmail.com>
2026-03-20 09:46:09 +0100

    Add proxy trust middleware for HTTPS support behind load balancer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust proxy headers for HTTPS behind load balancer/reverse proxy
+        $middleware->trustProxies(at: '*');
         // Future auth/admin middleware aliases can be registered here.
     })
     ->withExceptions(function (Exceptions $exceptions): void {
