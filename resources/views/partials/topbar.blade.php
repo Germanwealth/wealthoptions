61545ebcdd9385c9436e350a5c1f8e02397336cf
DjCeejay <lordceejay12@gmail.com>
2026-03-20 10:12:48 +0100

    Okay so how is it now?

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -1,28 +1,27 @@
 <div class="site-top">
     <div class="topbar-transparent" id="top-bar">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-9 col-md-9 col-sm-12">
-                    <ul class="top-info">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <ul class="top-info top-info-contact">
                         <li>
                             <span class="info-icon"><i class="icon icon-phone3"></i></span>
                             <div class="info-wrapper">
-                                <p class="info-title">
+                                <p class="info-title mb-0">
                                     <a href="https://api.whatsapp.com/send?phone=+18122005740">+18122005740</a>
                                 </p>
                             </div>
                         </li>
-                        <li class="last">
+                        <li>
                             <span class="info-icon"><i class="icon icon-envelope"></i></span>
                             <div class="info-wrapper">
-                                <p class="info-title">
+                                <p class="info-title mb-0">
                                     <a href="mailto:support@wealthoptions.com">support@wealthoptions.com</a>
                                 </p>
                             </div>
                         </li>
                     </ul>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-12 text-lg-right text-md-center"></div>
             </div>
         </div>
     </div>
